4e86ad67b7ef7d8d8147ea846f2ff5204472e52e
Jainish Jain <chngnddvlop@gmail.com>
2020-10-03 13:31:56 +0530

    update app

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 #import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-#from sklearn.externals import joblib
 import joblib
 
 
